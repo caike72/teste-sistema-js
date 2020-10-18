bb6a8f029938f3a5ab39e6da7205a93104ad4277
Caike Laurentino <caike_tb@ymail.com>
2020-10-18 20:17:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+            
+    //recuperar o valor contido no campo de texto
+    function verificaValores() {
+        var valor = document.getElementById('valorRecebido').value 
+
+        if (valor == 0) {
+            alert('Informe um valor válido')
+        }
+
+        console.log(valor)
+    }
+
+    //
+
